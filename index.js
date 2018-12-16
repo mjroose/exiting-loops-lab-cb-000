169431ee28cd8fff7586f3cb5e091d3a4fdafffb
Michael Roose <mjroose@gmail.com>
2018-12-16 18:19:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const breakOut = (array, changeValue, stopValue) => {
   array.forEach((element, index, array) => {
-    if (element === stopValue) break;
+    if (element === stopValue) {
+      break;
+    }
     array[index] = changeValue;
   });
   return array;
